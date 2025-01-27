bdd19a3f8ff07f7cfd469a95b47dda672a1e0b20
ayush-0012 <ayushbisht0012@gmail.com>
2025-01-27 12:38:58 +0530

    added a check for backend url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_BACKEND_URL;
-console.log(API_URL);
+const API_URL =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_DEV_BACKEND_URL
+    : import.meta.env.VITE_PROD_BACKEND_URL;
+
+console.log("Base URL:", API_URL);
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
